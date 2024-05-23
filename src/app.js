1278c4c4b125f33f6411b4e25a0a3a76fd64d62e
Akanksha Dubey <67544813+akanksha-web@users.noreply.github.com>
2024-05-23 21:49:53 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.get('/product/:productId', (req, res) => {
   //Write a code here for endpoint /product/:productId and also print parameter in json form
+  const productId = req.params.productId;
+  if(!productId){
+    return res.status(400).json({error: "productID is required"})
+  }
+  res.status(200).json({productId: productId});
 });
 
 module.exports = app;
